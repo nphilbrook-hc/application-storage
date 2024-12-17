782d5eba6f12d2384ecbea0bf5026becc9c38707
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-12-17 15:19:37 -0500

    force_destroy on because

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "simple_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "~>4.1.2"
-
+  
   bucket_prefix = "plugincachedebugcreatedbucket"
+  force_destroy = true
 }
