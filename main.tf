ac61f1761d7a9a57e6b4fbb66921ffc9b4d074b5
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-12-17 17:28:09 -0500

    rename

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ module "simple_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "~>4.1.2"
   
-  bucket_prefix = "plugincachedebugcreatedbucket"
+  bucket_prefix = "plugincachedebugcreatedbucket-foo"
   force_destroy = true
 }
